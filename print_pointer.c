e879f6f69c899880d350e68b5ad5e166195fa859
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-09 11:41:45 +0000

    kk

--- print_pointer.c
+++ print_pointer.c
@@ -2,20 +2,23 @@
 
 int print_pointer(void *p, char *buffer, int *buff_index)
 {
-    int count = 0;
+    int count;
+    unsigned long addr;
 
     /* NULL pointer */
     if (!p)
         return print_string("(nil)", buffer, buff_index);
 
-    unsigned long addr = (unsigned long)p;
+    /* bütün dəyişənləri funksiyanın başlanğıcında elan et */
+    addr = (unsigned long)p;
+    count = 0;
 
     /* add 0x prefix */
     buffer[*buff_index] = '0';
     (*buff_index)++;
     buffer[*buff_index] = 'x';
     (*buff_index)++;
-    count = 2;
+    count += 2;
 
     /* print hex address */
     count += print_number(addr, buffer, buff_index, 16, "0123456789abcdef");
