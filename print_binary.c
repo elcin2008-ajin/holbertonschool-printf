40fb33884158e863f080b3f6785be31ec350792e
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-09 11:51:02 +0000

    kk

--- print_binary.c
+++ print_binary.c
@@ -2,5 +2,5 @@
 
 int print_binary(unsigned int n, char *buffer, int *buff_index)
 {
-    return (print_number(n, buffer, buff_index, 2, "01"));
+    return print_number(n, buffer, buff_index, 2, "01", 0, 0);
 }
